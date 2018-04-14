1940a96036194125709885a458e192545844d9bf
Rodesigned <rodesigned@att.net>
2018-04-14 09:35:51 -0400

    upload files

---
+++ src/App.js
@@ -0,0 +1,75 @@
+// Clicky
+
+import React, { Component } from "react";
+import CharacterCard from "./components/CharacterCard";
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import Footer from "./components/Footer";
+import characters from "./characters.json";
+
+
+class App extends Component {
+
+  // Set our state variables
+  state = {
+    guessArray: [],
+    message: "Click an image to begin!",
+    score: 0,
+    topScore: 0,
+    // shake: 0
+  };
+
+  // Card is clicked
+  clickCard = card => {
+    let guessArray = this.state.guessArray;
+    let score = this.state.score;
+    // If we already clicked this card...
+    if (guessArray[card.id]) {
+      this.setState({
+        message: "You already guessed that! Starting over!",
+        topScore: Math.max(this.state.score, this.state.topScore),
+        guessArray: [],
+        score: 0,
+        
+      })
+      // Otherwise it was a good guess!
+    } else {
+      guessArray[card.id] = true;
+      this.setState({
+        message: "Good Guess!",
+        guessArray: guessArray,
+        score: ++score,
+        
+      })
+    }
+  };
+
+  // Render the page
+  render() {
+    return (
+      <div>
+        <Navbar
+          message={this.state.message}
+          score={this.state.score}
+          topScore={this.state.topScore} />
+        <Jumbotron />
+        
+          <Wrapper>
+            {characters
+              .sort((a, b) => 0.5 - Math.random())
+              .map(randomCard => (
+                <CharacterCard
+                  clickCard={this.clickCard}
+                  id={randomCard.id}
+                  key={randomCard.id}
+                  image={randomCard.image} />))}
+          </Wrapper>
+        
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+// Footer component
+const Footer = props => (
+    <div class="footer-copyright py-3 text-center text-white">
+        &copy; Copyright 2018 Rosita Cabrera
+    </div>
+);
+
+export default Footer;
